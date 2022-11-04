1928e787e06e64232ffd20e7e999f89947da7388
joyersol <joyer1339@gmail.com>
2022-11-04 11:20:54 -0400

    init docker-compose

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('/aboba/:id/')
+  getHello(@Param('id') id: number): string {
     return this.appService.getHello();
   }
 }
