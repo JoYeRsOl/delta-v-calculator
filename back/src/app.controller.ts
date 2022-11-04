01b426d023375ddbc4d447a5cdcf902438ae83e9
joyersol <joyer1339@gmail.com>
2022-11-04 11:47:26 -0400

    fix: fix docker compose conf

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -1,10 +1,9 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Get, Inject, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(@Inject(AppService) private readonly appService: AppService) {}
   @Get('/aboba/:id/')
   getHello(@Param('id') id: number): string {
     return this.appService.getHello();
